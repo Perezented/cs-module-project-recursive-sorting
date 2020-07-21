0d160a2a5492afa32feb9ce3dc4703ceccb420a3
Michael Perez <mperez6473@gmail.com>
2020-07-21 14:47:47 -0500

    working on getting the merge setup to not throw all 0s

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -4,7 +4,8 @@ def merge(arrA, arrB):
     merged_arr = [0] * elements
 
     # Your code here
-
+    print(f'arrA:', arrA)
+    print(f'arrB:', arrB)
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
@@ -12,9 +13,26 @@ def merge(arrA, arrB):
 
 def merge_sort(arr):
     # Your code here
-
+    # if the length of the array is 1
+    if len(arr) == 1:
+        # return array
+        return arr
+    # midpoint
+    midpoint = len(arr) // 2
+    # left side
+    a1 = arr[midpoint:]
+    # right side
+    a2 = arr[:midpoint]
+    a1 = merge_sort(a1)
+    a2 = merge_sort(a2)
+    print(a1)
+    print(a2)
+    return merge(a1, a2)
     return arr
 
+
+testingArray = [5, 87, 6, 2, 1, 4, 7, 54]
+print(merge_sort(testingArray))
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists
@@ -23,7 +41,9 @@ def merge_sort(arr):
 
 def merge_in_place(arr, start, mid, end):
     # Your code here
+    return
 
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
+    return
