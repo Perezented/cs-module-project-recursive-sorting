e7ba4f20fda2cdf8a388f04fddec84f56c38cd7f
Michael Perez <mperez6473@gmail.com>
2020-07-21 17:40:17 -0500

    More attempts at searching

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -5,14 +5,32 @@ testingArray = []
 def binary_search(arr, target, start, end):
     # Your code here
     # find the midpoint and check to see if the target is equal to, greater than, or less than the mid area.if not split the next half in half and see if the target is less than, greater than or equal to it.
-    midpoint = start + end // 2
-    if arr[midpoint] == target:
-        return midpoint
-    if target < arr[midpoint]:
-        binary_search(arr[midpoint:], target, midpoint, end)
-    else:
-        binary_search(arr[:midpoint], target, start, midpoint)
-    return
+    midpoint = (start + end) // 2
+    # print(arr)
+    if len(arr) == 1:
+        return arr
+        if target == arr[midpoint]:
+            return midpoint
+    if target == arr[-1]:
+        return len(arr)-1
+    elif target > arr[midpoint]:
+        # print(arr[midpoint:])
+        binary_search(arr[midpoint:], target, start, len(arr[midpoint:]))
+    elif target < arr[midpoint]:
+        # print(arr[:midpoint])
+        binary_search(arr[:midpoint], target, start, len(arr[:midpoint]))
+
+    # if len(arr) == 0:
+    #     return -1
+    # if len(arr) == 1:
+    #     print(arr)
+    # else:
+    #     if arr[midpoint + 1] >= target:
+    #         binary_search(arr[start: midpoint], target, start, midpoint)
+    #     if arr[midpoint] < target:
+    #         end = end - midpoint
+    #         binary_search(arr[midpoint:end], target, midpoint, end)
+
     # STRETCH: implement an order-agnostic binary search
     # This version of binary search should correctly find
     # the target regardless of whether the input array is
